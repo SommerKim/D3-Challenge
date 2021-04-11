b71eb2c39aa1660992d705213e2fd78f065837d5
SommerKim <74261427+SommerKim@users.noreply.github.com>
2021-04-11 17:38:29 -0400

    Created axes labels

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -86,5 +86,18 @@ d3.csv("data.csv").then(function(data) {
         });
     
     // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 10)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Household Income (Median)");
 
-}
+    chartGroup.append("text")
+        .attr("transform", `translate(${width/2}, ${height + margin.top + 10})`)
+        .attr("class", "axisText")
+        .text("Obesity (%)");
+    }).catch(function(error)) {
+        console.log(error);
+    });
